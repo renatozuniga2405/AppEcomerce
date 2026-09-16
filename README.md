311c01f89f241020e88722851a4bc852c689091d
Renato Zuniga <rzunigabermejo@gmail.com>
2026-09-15 23:08:24 -0500

    feat: configure astro site, base and github pages workflow

--- README.md
+++ README.md
@@ -79,3 +79,4 @@ AppEcomerce/
 ## 📄 Licencia
 
 MIT © 2025 ATELIER Fashion House.
+
--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://renatozuniga2405.github.io',
+  base: '/AppEcomerce',
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
   ],
 });
-
